60ddc443e7c6cd0d4570f4619e48433d8312bf42
MatiasFernandez <mfernandez@10pines.com>
2019-05-08 12:08:23 -0300

    Cleanups

--- lib/lhm/connection.rb
+++ lib/lhm/connection.rb
@@ -6,11 +6,11 @@ module Lhm
     SESSION_WAIT_LOCK_TIMEOUT = LONG_QUERY_TIME_THRESHOLD + INITIALIZATION_DELAY + TRIGGER_MAXIMUM_DURATION
     TABLES_WITH_LONG_QUERIES = %w(designs campaigns campaign_roots tags orders).freeze
 
-    def execute_metadata_locking_statements(statements, table, on_error)
-      kill_long_running_queries_on_origin_table!(table)
+    def execute_metadata_locking_statements(statements, table, on_error = nil)
+      kill_long_running_queries(table) if usually_has_long_queries?(table)
       with_transaction_timeout(on_error: on_error) do
         statements.each do |statement|
-          kill_long_running_queries_during_transaction(table) do
+          kill_long_blocking_queries_while_statement_is_running(table) do
             execute(statement)
           end
         end
@@ -18,9 +18,8 @@ module Lhm
     end
 
     def with_transaction_timeout(on_error: nil)
-      lock_wait_timeout = ar_connection.execute("SHOW SESSION VARIABLES WHERE VARIABLE_NAME='LOCK_WAIT_TIMEOUT'").to_a.flatten[1].to_i
-      ar_connection.execute("SET SESSION LOCK_WAIT_TIMEOUT=#{SESSION_WAIT_LOCK_TIMEOUT}")
-      Lhm.logger.info "Set transaction timeout (SESSION LOCK_WAIT_TIMEOUT) to #{SESSION_WAIT_LOCK_TIMEOUT} seconds."
+      lock_wait_timeout = get_session_timeout
+      set_session_timeout(SESSION_WAIT_LOCK_TIMEOUT)
       yield
     rescue => e
       if on_error.present?
@@ -33,15 +32,45 @@ module Lhm
         raise
       end
     ensure
-      ar_connection.execute("SET SESSION LOCK_WAIT_TIMEOUT=#{lock_wait_timeout}")
-      Lhm.logger.info "Set transaction timeout (SESSION LOCK_WAIT_TIMEOUT) back to #{lock_wait_timeout} seconds."
+      set_session_timeout(lock_wait_timeout)
     end
 
-    def kill_long_running_queries_on_origin_table!(table, connection: nil)
-      return unless killing_queries_enabled? && usually_has_long_queries?(table)
+    private
+
+    def ar_connection
+      __getobj__
+    end
+
+    def get_session_timeout
+      ar_connection.select_one("SHOW SESSION VARIABLES LIKE 'LOCK_WAIT_TIMEOUT'")["Value"].to_i
+    end
+
+    def set_session_timeout(new_timeout)
+      ar_connection.execute("SET SESSION LOCK_WAIT_TIMEOUT=#{new_timeout}")
+      Lhm.logger.info "Set transaction timeout (SESSION LOCK_WAIT_TIMEOUT) to #{new_timeout} seconds."
+    end
+
+    def kill_long_blocking_queries_while_statement_is_running(table)
+      t = Thread.new do
+        if killing_queries_enabled?
+          # the goal of this thread is to kill queries on the table specified that may be blocking metadata_lock
+          # adquisition. These queries started before the current metadata locking statement started
+          # We delay query killing to confirm the statement we want to protect got actually blocked.
+          sleep(LONG_QUERY_TIME_THRESHOLD + INITIALIZATION_DELAY)
+          new_connection = ActiveRecord::Base.connection
+
+          kill_long_running_queries(table, connection: new_connection)
+        end
+      end
+      yield
+      t.join
+    end
+
+    def kill_long_running_queries(table, connection: nil)
+      return unless killing_queries_enabled?
 
       connection ||= ar_connection
-      long_running_queries(table.name, connection: connection).each do |id, query, duration|
+      long_running_queries(table.name, connection).each do |id, query, duration|
         Lhm.logger.info "Action on table #{table.name} detected; killing #{duration}-second query: #{query}."
         begin
           connection.execute("KILL #{id};")
@@ -55,22 +84,6 @@ module Lhm
       end
     end
 
-    def kill_long_running_queries_during_transaction(table)
-      t = Thread.new do
-        if killing_queries_enabled?
-          # the goal of this thread is to wait until long running queries that started between
-          # #kill_long_running_queries_on_origin_table! and trigger creation
-          # to pass the threshold time and then kill them
-          sleep(LONG_QUERY_TIME_THRESHOLD + INITIALIZATION_DELAY)
-          new_connection = ActiveRecord::Base.connection
-
-          kill_long_running_queries_on_origin_table!(table, connection: new_connection)
-        end
-      end
-      yield
-      t.join
-    end
-
     def killing_queries_enabled?
       ENV['LHM_KILL_LONG_RUNNING_QUERIES'] == 'true'
     end
@@ -79,8 +92,7 @@ module Lhm
       TABLES_WITH_LONG_QUERIES.include? table.name
     end
 
-    def long_running_queries(table_name, connection: nil)
-      connection ||= ar_connection
+    def long_running_queries(table_name, connection)
       result = connection.execute <<-SQL.strip_heredoc
         SELECT ID, INFO, TIME FROM INFORMATION_SCHEMA.PROCESSLIST
         WHERE command <> 'Sleep'
@@ -90,12 +102,6 @@ module Lhm
           AND TIME > '#{LONG_QUERY_TIME_THRESHOLD}'
       SQL
       result.to_a.compact
-    end
-
-    private
-
-    def ar_connection
-      __getobj__
     end
   end
 end
